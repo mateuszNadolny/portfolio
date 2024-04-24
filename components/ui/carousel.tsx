3fc825d31a1d1886437544808e7dca14faa1bcdc
mateuszNadolny <mateusznadolny00@gmail.com>
2024-04-24 19:54:55 +0200

    style: adjust position of carousel arrows

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -186,7 +186,7 @@ const CarouselPrevious = React.forwardRef<HTMLButtonElement, React.ComponentProp
         className={cn(
           'absolute  h-8 w-8 rounded-full hover:bg-transparent focus:transparent',
           orientation === 'horizontal'
-            ? '-left-8 top-1/2 -translate-y-1/2'
+            ? '-left-2 top-1/2 -translate-y-1/2'
             : '-top-8 left-1/2 -translate-x-1/2 rotate-90',
           className
         )}
@@ -213,7 +213,7 @@ const CarouselNext = React.forwardRef<HTMLButtonElement, React.ComponentProps<ty
         className={cn(
           'absolute h-8 w-8 rounded-full hover:bg-transparent focus:transparent',
           orientation === 'horizontal'
-            ? '-right-8 top-1/2 -translate-y-1/2'
+            ? '-right-2 top-1/2 -translate-y-1/2'
             : '-bottom-8 left-1/2 -translate-x-1/2 rotate-90',
           className
         )}
