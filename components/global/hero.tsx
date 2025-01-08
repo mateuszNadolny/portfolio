5c21710eac914049142d736292473f2db3a6d220
mateuszNadolny <mateusznadolny00@gmail.com>
2025-01-08 18:07:28 +0100

    feat: Enhance Hero component with new animations and styling
    - Updated z-index for the Hero button to improve layering.
    - Added two new motion divs with animations for enhanced visual effects.
    - Introduced a new image with opacity settings for better integration in the Hero section.
    - Improved responsiveness and animation transitions for a more dynamic user experience.

--- components/global/hero.tsx
+++ components/global/hero.tsx
@@ -47,12 +47,110 @@ const Hero = () => {
           damping: 12,
           delay: 0.4,
         }}
-        className="z-20 bg-gradient-to-b from-neutral-400 to-neutral-200 text-[#0f1010] text-xs sm:text-base px-2 py-2 md:px-4 md:py-2 rounded-xl font-noto font-[400] hover:bg-gradient-to-b hover:from-neutral-300 hover:to-neutral-100"
+        className="z-30 bg-gradient-to-b from-neutral-400 to-neutral-200 text-[#0f1010] text-xs sm:text-base px-2 py-2 md:px-4 md:py-2 rounded-xl font-noto font-[400] hover:bg-gradient-to-b hover:from-neutral-300 hover:to-neutral-100"
       >
         {`Let's build your online presence together`}
       </motion.button>
       <motion.div
         className="absolute w-[50px] h-[50px] lg:w-[420px] lg:h-[420px] top-24 md:top-20 right-auto lg:top-auto lg:bottom-32 lg:right-[4vw] xl:right-[8vw] 2xl:right-[12vw] z-20 opacity-[10%] sm:opacity-100"
+        initial={{ opacity: 0, x: 500 }}
+        animate={{ opacity: 1, x: 0 }}
+        whileInView={{
+          y: [-10, 50],
+          rotate: [3, -3],
+        }}
+        viewport={{ once: false }}
+        transition={{
+          // Entrance animations
+          x: { duration: 1, ease: "easeOut", type: "spring" },
+          opacity: { duration: 1, ease: "easeOut", type: "spring" },
+          // Continuous animations
+          y: {
+            duration: 4,
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatType: "reverse",
+          },
+          rotate: {
+            duration: 4,
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatType: "reverse",
+          },
+        }}
+      >
+        <Image
+          src="/hero2.png"
+          alt="Hero Image"
+          width={500}
+          height={500}
+          className="absolute opacity-30 sm:opacity-90 z-20"
+        />
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: [0, 0.3],
+          }}
+          transition={{
+            duration: 4,
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatType: "reverse",
+          }}
+          className="absolute w-[100px] h-[100px] lg:w-[400px] lg:h-[400px] rounded-full blur-3xl bg-white z-10 opacity-[10%] sm:opacity-100"
+        />
+      </motion.div>
+      <motion.div
+        className="absolute block sm:hidden w-[50px] h-[50px] lg:w-[420px] lg:h-[420px] bottom-24 md:top-20 right-12 lg:top-auto lg:bottom-32 lg:right-[4vw] xl:right-[8vw] 2xl:right-[12vw] z-20 opacity-[10%] sm:opacity-100"
+        initial={{ opacity: 0, x: 500 }}
+        animate={{ opacity: 1, x: 0 }}
+        whileInView={{
+          y: [-10, 50],
+          rotate: [3, -3],
+        }}
+        viewport={{ once: false }}
+        transition={{
+          // Entrance animations
+          x: { duration: 1, ease: "easeOut", type: "spring" },
+          opacity: { duration: 1, ease: "easeOut", type: "spring" },
+          // Continuous animations
+          y: {
+            duration: 4,
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatType: "reverse",
+          },
+          rotate: {
+            duration: 4,
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatType: "reverse",
+          },
+        }}
+      >
+        <Image
+          src="/hero2.png"
+          alt="Hero Image"
+          width={500}
+          height={500}
+          className="absolute opacity-30 sm:opacity-90 z-20"
+        />
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: [0, 0.3],
+          }}
+          transition={{
+            duration: 4,
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatType: "reverse",
+          }}
+          className="absolute w-[100px] h-[100px] lg:w-[400px] lg:h-[400px] rounded-full blur-3xl bg-white z-10 opacity-[10%] sm:opacity-100"
+        />
+      </motion.div>
+      <motion.div
+        className="absolute block sm:hidden w-[50px] h-[50px] lg:w-[420px] lg:h-[420px] bottom-36 md:top-20 left-8 lg:top-auto lg:bottom-32 lg:right-[4vw] xl:right-[8vw] 2xl:right-[12vw] z-20 opacity-[10%] sm:opacity-100"
         initial={{ opacity: 0, x: 500 }}
         animate={{ opacity: 1, x: 0 }}
         whileInView={{
