a32f8bfd6ddcc8286fe0952368ba172276d41f88
mateuszNadolny <mateusznadolny00@gmail.com>
2025-01-24 13:20:40 +0100

    feat: Enhance SEO and update layout metadata in RootLayout
    - Updated title and meta description to better reflect services and improve search engine visibility.
    - Added various meta tags for keywords, author, and social media sharing to enhance discoverability.
    - Included structured data in JSON-LD format to provide search engines with detailed information about the developer.
    - Added new image (mnadolny.png) and removed outdated images (norway.png, tunis.png) from the public directory for better asset management.

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,13 +27,50 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="language" content="en" />
         <title>
-          Custom websites - Custom Solutions for Your Business Needs
+          Websites for business - Custom Solutions for Your Business Needs |
+          Mateusz Nadolny
         </title>
         <meta
           name="description"
-          content="Creating websites that turn visitors into customers. Custom websites, custom design, custom development. | Mateusz Nadolny | Fullstack Web Developer"
+          content="Experienced freelance web developer and designer specializing in modern, responsive websites and tailored solutions to grow your business."
         />
+        <meta
+          name="keywords"
+          content="websites, custom websites, wordpress, web page, landing page, business page, responsive websites, landing page for business, custom solutions, custom development, custom design, fullstack web developer, "
+        />
+        <meta name="author" content="Mateusz Nadolny" />
+        <meta name="robots" content="index, follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          property="og:title"
+          content="Websites for business - Custom Solutions for Your Business Needs | Mateusz Nadolny"
+        />
+        <meta
+          property="og:description"
+          content="Experienced freelance web developer and designer specializing in modern, responsive websites and tailored solutions to grow your business."
+        />
+        <meta
+          property="og:image"
+          content="https://raw.githubusercontent.com/mateuszNadolny/portfolio/refs/heads/main/public/mnadolny.png"
+        />
+        <meta property="og:url" content="https://mnadolny.com" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="Websites for business - Custom Solutions for Your Business Needs | Mateusz Nadolny"
+        />
+        <meta
+          name="twitter:description"
+          content="Experienced freelance web developer and designer specializing in modern, responsive websites and tailored solutions to grow your business."
+        />
+        <meta
+          name="twitter:image"
+          content="https://raw.githubusercontent.com/mateuszNadolny/portfolio/refs/heads/main/public/mnadolny.png"
+        />
+        <meta name="twitter:creator" content="@codeslav" />
+        <link rel="canonical" href="https://mnadolny.com" />{" "}
         <GoogleAnalytics gaId="G-63FF8KCRGJ" />
       </head>
       <ReactLenis root>
@@ -47,6 +84,26 @@ export default function RootLayout({
           <Toaster />
         </body>
       </ReactLenis>
+      <script type="application/ld+json">
+        {JSON.stringify({
+          "@context": "https://schema.org",
+          "@type": "Person",
+          name: "Mateusz Nadolny",
+          jobTitle: "Freelance Web Developer & Designer",
+          url: "https://mnadolny.com",
+          sameAs: [
+            "https://www.linkedin.com/in/mateusz-nadolny-web-dev/",
+            "https://github.com/mateuszNadolny",
+            "https://twitter.com/codeslav",
+          ],
+          worksFor: {
+            "@type": "Organization",
+            name: "Self-Employed",
+          },
+          description:
+            "Experienced freelance web developer specializing in custom, responsive website design and development to help businesses grow.",
+        })}
+      </script>
     </html>
   );
 }
